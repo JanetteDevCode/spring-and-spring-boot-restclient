df76b77bdfc3409db113bd6d9798d5cb04183d68
Janette Griggs <30553904+JanetteDevCode@users.noreply.github.com>
2020-05-04 11:00:55 -0700

    Obtain JSON response, deserialize, and return result

--- src/main/java/com/oreilly/restclient/services/JokeService.java
+++ src/main/java/com/oreilly/restclient/services/JokeService.java
@@ -1,5 +1,6 @@
 package com.oreilly.restclient.services;
 
+import com.oreilly.restclient.json.JokeResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -7,10 +8,22 @@ import org.springframework.web.client.RestTemplate;
 
 @Service
 public class JokeService {
+    private static final String BASE = "http://api.icndb.com/jokes/random?limitTo=[nerdy]";
+
     private RestTemplate restTemplate;
 
     @Autowired // not actually needed here since there's only one constructor
     public JokeService(RestTemplateBuilder builder) {
         restTemplate = builder.build();
+    }
+
+    public String getJoke(String first, String last) {
+        String url = String.format("%s&firstName=%s&lastName=%s", BASE, first, last);
+        JokeResponse response = restTemplate.getForObject(url, JokeResponse.class);
+        String output = "";
+        if (response != null) {
+            output = response.getValue().getJoke();
+        }
+        return output;
     }
 }

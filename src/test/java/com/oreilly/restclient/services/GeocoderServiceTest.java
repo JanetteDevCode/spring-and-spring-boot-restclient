5001b19a2055d5638073f75aa7ad58a518417d45
Janette Griggs <30553904+JanetteDevCode@users.noreply.github.com>
2020-05-04 12:40:23 -0700

    Obtain formatted address from JSON

--- src/test/java/com/oreilly/restclient/services/GeocoderServiceTest.java
+++ src/test/java/com/oreilly/restclient/services/GeocoderServiceTest.java
@@ -2,6 +2,8 @@ package com.oreilly.restclient.services;
 
 import com.oreilly.restclient.entities.Site;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,12 +11,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class GeocoderServiceTest {
+    private Logger logger = LoggerFactory.getLogger(GeocoderServiceTest.class);
+
     @Autowired
     private GeocoderService service;
 
     @Test
     void getLatLngForGoogleHeadquarters() {
         Site google = service.getLatLng("1600 Ampitheatre Parkway", "Mountain View", "CA");
+        logger.info(google.toString());
         assertEquals(37.42, google.getLatitude(), 0.01);
         assertEquals(-122.08, google.getLongitude(), 0.01);
     }
@@ -22,6 +27,7 @@ class GeocoderServiceTest {
     @Test
     void getLatLngWithoutStreet() {
         Site site = service.getLatLng("Boston", "MA");
+        logger.info(site.toString());
         assertEquals(42.36, site.getLatitude(), 0.01);
         assertEquals(-71.06, site.getLongitude(), 0.01);
     }
